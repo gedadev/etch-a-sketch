d2c2dc064d005ef9aad3092b92e47095b22efcee
gedadev <gjbg1234@gmail.com>
2022-02-23 21:32:32 -0600

    Added a button to reset the grid and generate a new one with a new size

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
 const container = document.querySelector('.container');
-let gridSize = 16;
+let gridSize = 0;
 let itemSize = 0;
 
-itemSize = parseInt(window.getComputedStyle(container).width) / gridSize;
+generateGrid();
 
-container.setAttribute('style', `grid-template-columns: repeat(${gridSize}, ${itemSize}px);`);
+function generateGrid(gridSize=16){
+    //Defining the size of the pixels in the grid
+    itemSize = parseInt(window.getComputedStyle(container).width) / gridSize;
+    container.setAttribute('style', `grid-template-columns: repeat(${gridSize}, ${itemSize}px);`);
 
-for(let i = 0; i < Math.pow(gridSize, 2); i++){
-    const pixel = document.createElement('div');
-    pixel.style.height = `${itemSize}px`;
-    pixel.className = "pixel";
-    container.appendChild(pixel);
+    //Generating the grid
+    for(let i = 0; i < Math.pow(gridSize, 2); i++){
+        const pixel = document.createElement('div');
+        pixel.style.height = `${itemSize}px`;
+        pixel.className = "pixel";
+        container.appendChild(pixel);
+    }
+    return document.querySelectorAll('.pixel');
 }
 
-const grid = document.querySelectorAll('.pixel');
-
+let grid = document.querySelectorAll('.pixel');
+//Changing the color of the divs
 grid.forEach(pixel => pixel.addEventListener('mouseover', (e) => {
-    //console.log(e);
-    pixel.className = "colored";
+    pixel.classList.add('colored');
 }));
+
+//Clear the grid and defining a new gridSize 
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', () => {
+    gridSize = prompt('New grid size');
+    grid.forEach(pixel => container.removeChild(pixel));
+    grid = generateGrid(gridSize);
+    grid.forEach(pixel => pixel.addEventListener('mouseover', (e) => {
+        pixel.classList.add('colored');
+    }));
+});
+
